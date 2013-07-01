6da737eaf3b61a62586c1026f056d22606d0994f
Claudio Schwarz <claudio@claudioschwarz.com>
2013-07-01 22:53:22 +0200

    Gameplan
    work in progress, not finished

--- widget/suhv_gameplan.php
+++ widget/suhv_gameplan.php
@@ -1,7 +1,32 @@
 <?php
 
 if ($games == 'on') {
+  $gamePlan = $team->getGames();
+
+  var_dump($gamePlan);
+
   $gamePlanHtml = '<div class="results-championship"><h4>Spielplan</h4>';
+  foreach ($gamePlan as $game) {
+    $gamePlanHtml .= ''
+      .'<div class="gameplan">'
+      . '<table border="0" cellpadding="0" cellspacing="0">'
+      .   '<tr class="hometeam">'
+      .     '<td>'
+      .       $game['round']
+      .     '</td>'
+      .     '<td class="hometeamname">'
+      .       $game['hometeamname']
+      .     '</td>'
+      .   '</tr>'
+      .   '<tr class="awayteam">'
+      .     '<td class="awayteamname">'
+      .       $game['awayteamname']
+      .     '</td>'
+      .   '</tr>'
+      . '</table>'
+      .'</div>';
+
+  }
   $gamePlanHtml .= '</div>';
   echo $gamePlanHtml;
 }
